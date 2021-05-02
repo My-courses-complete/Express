fb543c022488350594eb5ff63f9fb4fc13e1a7bd
Jesús Mejía <51868559+JMR-Mejia@users.noreply.github.com>
2021-05-02 17:49:46 -0500

    Template Engines-y-archivos-estáticos (#2)
    * Qué es y cómo crear tu propio template engine
    
    * Implementando un template engine
    
    * Usando Pug como template engine
    
    * Usando Handlebars como template engine
    
    * Creando nuestro layout de productos usando Pug
    
    * Manejo de archivos estáticos

---
+++ ecommerce/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const productsRouter = require("./routes/products");
+
+const app = express();
+
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use("/products", productsRouter);
+
+const server = app.listen(3000, () => {
+  console.log(`Listening http://localhost:${server.address().port}`);
+});
