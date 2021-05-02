2fe57c6e2a2371ced7545161697c78ecf13f8ba7
JMR-Mejia <chucho4720013@gmail.com>
2021-05-02 18:54:54 -0500

    Anatomía de una API RESTful e implementación CRUD

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const productsRouter = require("./routes/products");
+const productsApiRouter = require("./routes/api/products");
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use("/products", productsRouter);
+app.use("/api/products", productsApiRouter);
 
 const server = app.listen(3000, () => {
   console.log(`Listening http://localhost:${server.address().port}`);
