a989a63048fd92cadda86e9a54e33505adf52c06
JMR-Mejia <chucho4720013@gmail.com>
2021-05-02 00:17:40 -0500

    Implementando un template engine

--- template-engine/index.js
+++ template-engine/index.js
@@ -3,11 +3,11 @@ const app = express();
 const expressJsx = require("./express-jsx");
 
 app.engine("jsx", expressJsx);
-app.set("views", "/views");
-app.set("view engine", "jsx")
+app.set("views", "./views");
+app.set("view engine", "jsx");
 
 app.get("/", function (req, res) {
-  res.render(index, { hello: "hola", world: "world" });
+  res.render("index", { hello: "hola", world: "mundo genial" });
 });
 
 const server = app.listen(3000, function () {
