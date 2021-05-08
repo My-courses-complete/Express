292462944d9666d4c44d61ed226b0b6a73ee6eec
Jesús Mejía <51868559+JMR-Mejia@users.noreply.github.com>
2021-05-08 15:40:33 -0500

    Conectando Express.js-con-servicios-externos (#4)
    * Conectando nuestros servicios con mongo DB
    
    * Implementando acciones CRUD en MongoDB

--- ecommerce/services/products.js
+++ ecommerce/services/products.js
@@ -1,26 +1,43 @@
 const productMocks = require("../utils/mocks/products");
+const MongoLib = require("../lib/mongo");
 
 class ProductsService {
-  constructor() {}
-
-  getProducts({ tags }) {
-    return Promise.resolve(productMocks);
+  constructor() {
+    this.collection = "products";
+    this.mongoDB = new MongoLib();
   }
 
-  getProduct({ productId }) {
-    return Promise.resolve(productMocks[0]);
+  async getProducts({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+    const products = await this.mongoDB.getAll(this.collection, query);
+    return products || [];
   }
 
-  createdProduct({ product }) {
-    return Promise.resolve(productMocks[0]);
+  async getProduct({ productId }) {
+    const product = await this.mongoDB.get(this.collection, productId);
+    return product || [];
   }
 
-  updateProduct({ productId, product }) {
-    return Promise.resolve(productMocks[0]);
+  async createdProduct({ product }) {
+    const createProductId = await this.mongoDB.create(this.collection, product);
+    return createProductId;
   }
 
-  deleteProduct({ productId }) {
-    return Promise.resolve(productMocks[0]);
+  async updateProduct({ productId, product }) {
+    const updateProductId = await this.mongoDB.update(
+      this.collection,
+      productId,
+      product
+    );
+    return updateProductId;
+  }
+
+  async deleteProduct({ productId }) {
+    const deleteProductId = await this.mongoDB.delete(
+      this.collection,
+      productId
+    );
+    return deleteProductId;
   }
 }
 
