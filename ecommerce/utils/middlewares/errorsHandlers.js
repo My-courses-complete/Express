937bed3935dc9da8520268b5a3db3468ec6c0dd2
JMR-Mejia <chucho4720013@gmail.com>
2021-05-08 17:22:41 -0500

    Implementando Sentry para manejo de log de errores

--- ecommerce/utils/middlewares/errorsHandlers.js
+++ ecommerce/utils/middlewares/errorsHandlers.js
@@ -1,6 +1,24 @@
 const config = require("../../config/index");
 
+const Sentry = require("@sentry/node");
+// or use es6 import statements
+// import * as Sentry from '@sentry/node';
+
+const Tracing = require("@sentry/tracing");
+// or use es6 import statements
+// import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: `https://${config.sentryDns}/${config.sentryId}`,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 function logErrors(err, req, res, next) {
+  Sentry.captureException(err);
   console.log(err.stack);
   next(err);
 }
